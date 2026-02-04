5bfd7f2df0f62460c7202cb6e1cd6a3b6c6f56b5
compassgo001-lab <compassgo001@gmail.com>
2026-02-05 00:40:14 +0200

    Create icons.js

---
+++ assets/icons.js
@@ -0,0 +1,6 @@
+const MAP_ICONS = {
+    city: "https://img.icons8.com/isometric/512/castle.png",
+    capital: "https://img.icons8.com/isometric/512/city.png",
+    mine: "https://img.icons8.com/isometric/512/mine-cart.png",
+    tower: "https://img.icons8.com/isometric/512/defense-tower.png"
+};
